77bdc2e165c780804da09ba909d5e25ab8a3f94c
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-09-12 09:27:44 -0700

    added service-worker.js and updated server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,12 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
